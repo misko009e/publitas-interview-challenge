aeed402e7febb779ab83865eb0569fdc91d5c0db
Marko Misura <marko.misura@toptal.com>
2025-09-12 20:25:25 +0200

    Cleaned up the vite template code, and setup a basic empty image gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
 import './App.scss';
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import ImageGallery from './components/ImageGallery';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <h1>Publitas Frontend Code Challenge</h1>
+      <ImageGallery />
+    </div>
   );
 }
 
